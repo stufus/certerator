70fdacfea669401c2d781b8dbd13acb5e4a78e86
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2016-02-25 23:06:31 +0000

    Added functions for clarity and structure

--- certerator.py
+++ certerator.py
@@ -1,4 +1,6 @@
 #/usr/bin/env python
+import os
+import sys
 import OpenSSL
 
 def certerator_config():
@@ -41,39 +43,52 @@ def openssl_generate_privatekey(size):
     key.generate_key(OpenSSL.crypto.TYPE_RSA, size)
     return key
 
-config_ca, config_cert = certerator_config()
+def generate_certificate(config_ca,key):
+    ca = OpenSSL.crypto.X509()
+    ca.set_version(2)
+    ca.set_serial_number(config_ca['serial'])
+    ca_subj = ca.get_subject()
+    ca_subj.commonName = config_ca['commonName']
+    ca_subj.stateOrProvinceName = config_ca['stateOrProvinceName']
+    ca_subj.localityName = config_ca['localityName']
+    ca_subj.organizationName = config_ca['organizationName']
+    ca_subj.organizationalUnitName = config_ca['organizationalUnitName']
+    ca_subj.emailAddress = config_ca['emailAddress']
+    ca_subj.countryName = config_ca['countryName']
+    ca.set_notBefore(config_ca['validfrom'])
+    ca.set_notAfter(config_ca['validto'])
+    return ca
 
-key = openssl_generate_privatekey(config_ca['keyfilesize'])
-
-ca = OpenSSL.crypto.X509()
-ca.set_version(2)
-ca.set_serial_number(config_ca['serial'])
-ca_subj = ca.get_subject()
-ca_subj.commonName = config_ca['commonName']
-ca_subj.stateOrProvinceName = config_ca['stateOrProvinceName']
-ca_subj.localityName = config_ca['localityName']
-ca_subj.organizationName = config_ca['organizationName']
-ca_subj.organizationalUnitName = config_ca['organizationalUnitName']
-ca_subj.emailAddress = config_ca['emailAddress']
-ca_subj.countryName = config_ca['countryName']
-ca.set_notBefore(config_ca['validfrom'])
-ca.set_notAfter(config_ca['validto'])
-ca.set_issuer(ca_subj)
-ca.set_pubkey(key)
-ca.add_extensions([
-  OpenSSL.crypto.X509Extension("basicConstraints", True,
+def generate_ca(config_ca):
+    key = openssl_generate_privatekey(config_ca['keyfilesize'])
+    ca = generate_certificate(config_ca,key)
+    ca.add_extensions([
+        OpenSSL.crypto.X509Extension("basicConstraints", True,
                                "CA:TRUE, pathlen:1"),
-  OpenSSL.crypto.X509Extension("keyUsage", True,
+        OpenSSL.crypto.X509Extension("keyUsage", True,
                                "keyCertSign, cRLSign"),
-  OpenSSL.crypto.X509Extension("subjectKeyIdentifier", False, "hash",
+        OpenSSL.crypto.X509Extension("subjectKeyIdentifier", False, "hash",
                                subject=ca),
     ])
-ca.add_extensions([
-  OpenSSL.crypto.X509Extension("authorityKeyIdentifier", False, "keyid:always",issuer=ca)
-  ])
-ca.sign(key, config_ca['hashalgorithm'])
+    ca.add_extensions([
+        OpenSSL.crypto.X509Extension("authorityKeyIdentifier", False, "keyid:always",issuer=ca)
+    ])
+    ca.set_issuer(ca.get_subject())
+    ca.set_pubkey(key)
+    ca.sign(key, config_ca['hashalgorithm'])
+    return ca, key
 
-open(config_ca['cert_filename'], "wb").write(
-            OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, ca))
-open(config_ca['cert_key'], "wb").write(
-            OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, key))
+if __name__ == "__main__":
+    sys.stdout.write("Certerator v0.1-pre1")
+    sys.stdout.write("Stuart Morgan (@ukstufus) <stuart.morgan@mwrinfosecurity.com>")
+    sys.stdout.flush()
+
+    config_ca, config_cert = certerator_config()
+    ca, key = generate_ca(config_ca)
+
+    open(config_ca['cert_filename'], "wb").write(
+                OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, ca))
+    open(config_ca['cert_key'], "wb").write(
+                OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, key))
+    sys.exit(0)
+
