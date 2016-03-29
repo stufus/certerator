bbc2c24bcf6a409b41459d6e3617674122755e3e
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2016-03-29 11:52:05 +0100

    reg to command line parser, fixed regexs

--- reg2cmd.py
+++ reg2cmd.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 #  Certerator 0.1-pre1
@@ -12,10 +12,31 @@ import os
 import sys
 import re
 
-if __name__ == "__main__":
-    try:
-        # Read the .reg file from STDIN
-        regfile = sys.stdin.readlines()
-    
-        for line in regfile:
-            
+# Read the .reg file from STDIN
+regfile = sys.stdin.readlines()
+   
+reg_path = ""
+reg_key = ""
+reg_binary = ""
+
+# Loop through each line
+for line in regfile:
+
+    # Try to match the location
+    header = re.search(r'^\[([^\\]+\\SOFTWARE\\Microsoft\\SystemCertificates\\[^\\]+\\Certificates)\\([A-Z0-9]+)\]\s*$', line)
+    if header != None:
+        reg_path = header.group(1)
+        reg_key = header.group(2)
+
+    # Now match the rest of the file
+    body = re.search(r'^\s*(?:"Blob"=hex:)?\s*([a-z0-9,]+)[,\\]*\s*$', line)
+    if body != None:
+        reg_binary += body.group(1).replace(',','')
+
+if reg_path and reg_key and reg_binary:
+    print "[+] Path: "+reg_path
+    print "[+] Key: "+reg_key
+    print "[+] Binary: "+reg_binary
+
+    print "\n"
+
