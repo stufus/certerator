84f1e30382dfb42a66c10408856a94bf8cb0dd2b
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2016-03-29 13:43:51 +0100

    Update

--- reg2cmd.py
+++ reg2cmd.py
@@ -15,6 +15,8 @@ import re
 # Read the .reg file from STDIN
 regfile = sys.stdin.readlines()
    
+reg_base = ""
+reg_base_ps = ""
 reg_path = ""
 reg_key = ""
 reg_binary = ""
@@ -23,20 +25,18 @@ reg_binary = ""
 for line in regfile:
 
     # Try to match the location
-    header = re.search(r'^\[([^\\]+\\SOFTWARE\\Microsoft\\SystemCertificates\\[^\\]+\\Certificates)\\([A-Z0-9]+)\]\s*$', line)
+    header = re.search(r'^\[([^\\]+)\\(SOFTWARE\\Microsoft\\SystemCertificates\\[^\\]+\\Certificates)\\([A-Z0-9]+)\]\s*$', line, re.IGNORECASE)
     if header != None:
-        reg_path = header.group(1)
-        reg_key = header.group(2)
+        reg_base = header.group(1)
+        reg_path = header.group(2)
+        reg_key = header.group(3)
 
     # Now match the rest of the file
     body = re.search(r'^\s*(?:"Blob"=hex:)?\s*([a-z0-9,]+)[,\\]*\s*$', line)
     if body != None:
         reg_binary += body.group(1).replace(',','')
 
-if reg_path and reg_key and reg_binary:
-    print "[+] Path: "+reg_path
-    print "[+] Key: "+reg_key
-    print "[+] Binary: "+reg_binary
-
+if reg_base and reg_path and reg_key and reg_binary:
+    print "Command line reg.exe:"
+    print "reg add %s\\%s\\%s /v Blob /t REG_BINARY /d %s /f /reg:64" % (reg_base, reg_path, reg_key, reg_binary)
     print "\n"
-
